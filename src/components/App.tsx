b3d7011164c01e20669fd8ed68ad119b163698e4
Raziur Rahman <instrumaniak@gmail.com>
2019-08-30 13:45:50 +0600

    Added progress bar for API data loading indication

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,34 @@
-import React, { useState, Fragment } from "react"
+import React, { useState } from "react"
 import _ from "lodash"
 
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
-import Typography from "@material-ui/core/Typography"
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from "@material-ui/core/Toolbar"
-import Container from '@material-ui/core/Container'
-import CssBaseline from '@material-ui/core/CssBaseline'
+import LinearProgress from "@material-ui/core/LinearProgress"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { createMuiTheme } from "@material-ui/core/styles"
+import { StylesProvider } from "@material-ui/styles"
 
-import ListItem from './ListItem'
+import { ThemeProvider } from "styled-components"
+
+import ListGroup from "./ListGroup"
+import NavBar from "./NavBar"
+
+const defaultTheme = createMuiTheme()
 
 const App: React.FC = () => {
   const [input, setInput] = useState<string>("") // search text input
   const [results, setResults] = useState<any[]>([]) // results data from API
+  const [isLoading, setIsLoading] = useState<boolean>(false) // show loading bar
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value)
   }
 
   const handleKeys = (kinput: React.KeyboardEvent<HTMLInputElement>) => {
-    if(kinput.key === "Enter"){
+    if (kinput.key === "Enter") {
       getResults()
     }
-    if (kinput.key === "Escape"){
+    if (kinput.key === "Escape") {
       clear()
     }
   }
@@ -34,10 +39,12 @@ const App: React.FC = () => {
   }
 
   const getResults = async () => {
+    setIsLoading(false)
     const URL =
       "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search="
 
     if (input.trim().length > 0) {
+      setIsLoading(true)
       const res = await fetch(URL + input)
       const json = await res.json()
       const data = _.zip(json[1], json[2], json[3]).map(i =>
@@ -45,83 +52,49 @@ const App: React.FC = () => {
       )
 
       setResults(data)
+      setIsLoading(false)
     }
   }
-  
+
   return (
-    <>
-      <CssBaseline />
-      <AppBar>
-        <Toolbar>
-          <Typography variant="h6">
-            Search Wikipedia
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={defaultTheme}>
+        <>
+          <CssBaseline />
+          { isLoading && <LinearProgress style={{ zIndex: 2000 }} /> }
+          <NavBar />
 
-      <Container maxWidth="md">  
-        <div className="wiki-box">
-          <TextField
-            label="What do you want to search?..."
-            value={input}
-            onChange={handleInput}
-            onKeyUp={handleKeys}
-            fullWidth
-          />
+          <div className="wiki-box">
+            <TextField
+              label="What do you want to search?..."
+              value={input}
+              onChange={handleInput}
+              onKeyUp={handleKeys}
+              fullWidth
+            />
 
-          <div className="wiki-btns">
-            <Button
-              variant="outlined"
-              color="secondary"
-              onClick={clear}
-            >
-              Clear
-            </Button>{" "}
-            <Button
-              variant="outlined"
-              onClick={() =>
-                window.open("https://en.wikipedia.org/wiki/Special:Random")
-              }
-            >
-              Random
-            </Button>{" "}
-            
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={getResults}
-            >
-              Search
-            </Button>
+            <div className="wiki-btns">
+              <Button variant="outlined" color="secondary" onClick={clear}>
+                Clear
+              </Button>{" "}
+              <Button
+                variant="outlined"
+                onClick={() =>
+                  window.open("https://en.wikipedia.org/wiki/Special:Random")
+                }
+              >
+                Random
+              </Button>{" "}
+              <Button variant="outlined" color="primary" onClick={getResults}>
+                Search
+              </Button>
+            </div>
+
+            <ListGroup results={results} />
           </div>
-          <br />
-          <br />
-
-          <div className="list-group">
-            {results.length > 0 && (
-              <div>
-                <Typography variant="h4" gutterBottom>
-                  Results
-                </Typography>
-                <br />
-                {results.map((item, idx) => {
-                  return (
-                    <Fragment key={idx}>
-                      <ListItem
-                        link={item.link}
-                        title={item.title}
-                        info={item.info}
-                      />
-                    </Fragment>
-                  )
-                })}
-              </div>
-            )}
-          </div>
-        </div>
-      </Container>
-    </>
+        </>
+      </ThemeProvider>
+    </StylesProvider>
   )
 }
 
