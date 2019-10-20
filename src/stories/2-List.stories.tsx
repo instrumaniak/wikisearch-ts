efae85dc201a976b54cddd90e1c3e60129f2bc8a
Raziur Rahman <instrumaniak@gmail.com>
2019-10-20 21:21:55 +0600

    added storybook with typescript support

---
+++ src/stories/2-List.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import ListItem from "../components/ListItem"
+
+export default {
+  title: "List"
+}
+
+export const list = () => (
+  <ListItem
+    link="/"
+    title="Basic List item"
+    info="Lorem ipsum dolores Lorem ipsum dolores Lorem ipsum dolores"
+  />
+)
